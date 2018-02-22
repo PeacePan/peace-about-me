69b669e7c6bd0d3a0f62a7e3f8dfc01605290b81
PeacePan <minghe0915@gmail.com>
2018-02-22 21:33:44 +0800

    add(express): 新增靜態 http server

---
+++ http-server.js
@@ -0,0 +1,10 @@
+const express = require('express');
+const compression = require('compression');
+const path = require('path');
+const PORT = process.env.PORT || 9527;
+
+express()
+  .use(express.static(path.join(__dirname, 'build')))
+  .use(compression())
+  .get('/*', (req, res) => res.sendFile(path.join(__dirname, 'build', 'index.html')))
+  .listen(PORT, () => console.log(`http server is listening on http://localhost:${ PORT }/`));
